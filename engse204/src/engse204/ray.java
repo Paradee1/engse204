3ad519f3fedc9779c67ade8d983e1ad03e4e9d45
paradee1 <paradee5759@gmail.com>
2025-11-22 19:07:55 +0700

    wq

---
+++ engse204/src/engse204/ray.java
@@ -0,0 +1,32 @@
+package engse204;
+
+import java.util.Scanner;
+
+public class ray {
+
+	public static void main(String[] args) {
+		
+		Scanner ar=new Scanner(System.in);
+		
+		int n=ar.nextInt();
+		
+		int[] num = new int[n];   
+		
+		for(int i=0;i<n;i++) {
+			num[i]=ar.nextInt();
+		}
+		
+		int arr=sumArray(num);
+		
+		System.out.println(arr);
+		
+	}
+	static int sumArray(int[] number) {
+		int sum = 0;
+		for(int a=0;a<number.length;a++) {
+			sum += number[a];
+		}
+		return sum;
+	}
+
+}
