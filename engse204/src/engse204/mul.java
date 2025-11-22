8c8ce86e961f1753cc34e9dad71db46ac3bd6a92
paradee1 <paradee5759@gmail.com>
2025-11-22 16:28:29 +0700

    tt

--- engse204/src/engse204/mul.java
+++ engse204/src/engse204/mul.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class mul {
 
-	public static void main(String[] args) {
+	public static void main(String[] rgs) {
 		Scanner ab = new Scanner(System.in);
+		
 		int num=ab.nextInt();
 		
 		for(int i=1;i<=12;i++) {
