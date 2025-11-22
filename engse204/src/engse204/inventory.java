068fab0020cb4c259d5c79f5df3f3a4e534dc696
paradee1 <paradee5759@gmail.com>
2025-11-22 22:15:01 +0700

    tq

---
+++ engse204/src/engse204/inventory.java
@@ -0,0 +1,39 @@
+package engse204;
+
+import java.util.Scanner;
+
+public class inventory {
+
+	public static void main(String[] rgs) {
+		Scanner nv=new Scanner(System.in);
+				
+		int n=nv.nextInt();
+		
+		int[] productID=new int[n];
+		int[] Stock=new int[n];
+		
+		
+		for(int i=0;i<n;i++) {
+			productID[i]=nv.nextInt();
+			Stock[i]=nv.nextInt();
+		}
+		
+		int SearchID=nv.nextInt();
+		
+		boolean check = false;
+		for(int a=0;a<n;a++) {
+			if(SearchID == productID[a]) {
+				System.out.println(Stock[a]);
+				check = true;
+				break;
+			}
+		}
+		
+		if(!check) {
+			System.out.println("Product "+SearchID+" not found");
+		}
+		
+		
+	}
+
+}
--- engse204/src/engse204/ray.java
+++ engse204/src/engse204/ray.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ray {
 
-	public static void main(String[] args) {
+	public static void main(String[] rgs) {
 		
 		Scanner ar=new Scanner(System.in);
 		
