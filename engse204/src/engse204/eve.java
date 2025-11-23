cad507e93cb5bbdf5135eb62fb1c5b3a91fa22f0
paradee1 <paradee5759@gmail.com>
2025-11-23 10:40:38 +0700

    we

--- engse204/src/engse204/eve.java
+++ engse204/src/engse204/eve.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class eve {
 
-	public static void main(String[] args) {
+	public static void main(String[] rgs) {
 		Scanner ve=new Scanner(System.in);
 		
 		int num=ve.nextInt();
