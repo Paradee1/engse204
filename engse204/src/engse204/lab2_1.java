5d539bc43462e6839bd1901ac6ccd6e9d0909bae
paradee1 <paradee5759@gmail.com>
2025-11-29 16:30:00 +0700

    ds

--- engse204/src/engse204/lab2_1.java
+++ engse204/src/engse204/lab2_1.java
@@ -3,21 +3,7 @@ package engse204;
 import java.util.Scanner;
 
 public class lab2_1 {
-	
-	public static class Student {
-		private String studentId;
-		private String name;
-		
-		public Student(String studentId, String name) {
-			this.studentId = studentId;
-			this.name = name;
-	}
-		public void display() {
-			System.out.println(studentId);
-			System.out.println(name);
-		}
-}
-	
+
 	public static void main(String[] rgs) {
 		Scanner se=new Scanner(System.in);
 		
@@ -26,8 +12,19 @@ public class lab2_1 {
 		
 		Student st = new Student(id, name);
 		
-		st.display();
+		System.out.println(st.studentId);
+		System.out.println(st.name);
 
+	}
+
+}
+class Student {
+		public String studentId;
+		public String name;
+		
+		public Student(String studentId, String name) {
+			this.studentId = studentId;
+			this.name = name;
 	}
 
 }
