58d3757faef1af76db7b9c3dfc8452eddbc203a6
paradee1 <paradee5759@gmail.com>
2025-11-30 21:48:19 +0700

    asda

--- engse204/src/Lab2_7/Lab2_7.java
+++ engse204/src/Lab2_7/Lab2_7.java
@@ -5,48 +5,45 @@ import java.util.Scanner;
 public class Lab2_7 {
 
 	public static void main(String[] rgs) {
-		Scanner su=new Scanner(System.in);
+		Scanner su = new Scanner(System.in) ;
 		
-		System.out.print("Name:");
-		String name=su.nextLine();
-		System.out.print("ยอดเงินเริ่มต้น:");
-		double balance=su.nextDouble();
-		System.out.print("จำนวนเงินที่จะถอนครั้งที่ 1:");
-		double ws1=su.nextDouble();
-		System.out.print("จำนวนเงินที่จะถอนครั้งที่ 2:");
-		double ws2=su.nextDouble();
+		String name = su.nextLine() ;
+		double balance = su.nextDouble() ;
+		double ws1 = su.nextDouble() ;
+		double ws2 = su.nextDouble() ;
 		
-		BankAccount wi=new BankAccount(name, balance);
+		BankAccount wi = new BankAccount(name, balance) ;
 		
-		wi.withdraw(ws1);
-		wi.withdraw(ws2);
+		wi.withdraw(ws1) ;
+		wi.withdraw(ws2) ;
 		
-		wi.displayBalance();
+		wi.displayBalance() ;
 
 	}
 
 }
 
 class BankAccount {
-	private String ownerName;
-	private double balance;
 	
-	public BankAccount(String ownerName,double initialBalance) {
-		this.ownerName=ownerName;
-		this.balance=initialBalance;
+	private String ownerName ;
+	private double balance ;
+	
+	public BankAccount(String ownerName, double initialBalance) {
+		this.ownerName = ownerName ;
+		this.balance = initialBalance ;
 	}
 	
 	public void withdraw(double amount) {
 		if(amount <= balance) {
-			balance -= amount;
-			System.out.println("Withdraw successful.");
+			balance -= amount ;
+			System.out.println( "Withdraw successful." ) ;
 		}
 		else if(amount > balance) {
-			System.out.println("Insufficient funds.");
+			System.out.println( "Insufficient funds." ) ;
 		}
 	}
 	
 	public void displayBalance() {
-		System.out.println("Balance:"+balance);
+		System.out.println( "Balance:" + balance ) ;
 	}
 }
