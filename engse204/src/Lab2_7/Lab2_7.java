ecc95e4d057574a140f94718febec6846460b30f
paradee1 <paradee5759@gmail.com>
2025-12-01 00:06:35 +0700

    asdasdas

--- engse204/src/Lab2_7/Lab2_7.java
+++ engse204/src/Lab2_7/Lab2_7.java
@@ -7,16 +7,29 @@ public class Lab2_7 {
 	public static void main(String[] rgs) {
 		Scanner su = new Scanner(System.in) ;
 		
+		//รับข้อมูลชิ่อ
+		System.out.print( "Enter OwnerName: " ) ;
 		String name = su.nextLine() ;
+		
+		//รับยอดเงินในบัญชี
+		System.out.print( "Enter InitialBalance: " ) ;
 		double balance = su.nextDouble() ;
+		
+		//จำนวนเงินที่จะถอนรอบที่1
+		System.out.print( "Enter WithdrawalAmount 1: " ) ;
 		double ws1 = su.nextDouble() ;
+		
+		//จำนวนเงินที่จะถอนรอบที่2
+		System.out.print( "Enter WithdrawalAmount 2: " ) ;
 		double ws2 = su.nextDouble() ;
 		
 		BankAccount wi = new BankAccount(name, balance) ;
 		
+		//ทำรายการถอนเงิน
 		wi.withdraw(ws1) ;
 		wi.withdraw(ws2) ;
 		
+		//แสดงยอดเงินคงเหลือ
 		wi.displayBalance() ;
 
 		su.close();
@@ -26,14 +39,17 @@ public class Lab2_7 {
 
 class BankAccount {
 	
+	// Private Instance Attributes
 	private String ownerName ;
 	private double balance ;
 	
+	// Constructor
 	public BankAccount(String ownerName, double initialBalance) {
 		this.ownerName = ownerName ;
 		this.balance = initialBalance ;
 	}
 	
+	// Method: withdraw(double amount)
 	public void withdraw(double amount) {
 		
 		if(amount <= balance) {
@@ -45,6 +61,7 @@ class BankAccount {
 		}
 	}
 	
+	// Method: displayBalance()
 	public void displayBalance() {
 		System.out.println( "Balance:" + balance ) ;
 	}
