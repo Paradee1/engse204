cf3e46c5a1e0017f7550e5c0ddb71f5c2d78c7e5
paradee1 <paradee5759@gmail.com>
2025-11-30 21:41:12 +0700

    asd

---
+++ engse204/src/Lab2_8/Lab2_8.java
@@ -0,0 +1,37 @@
+package Lab2_8;
+
+import java.util.Scanner;
+
+public class Lab2_8 {
+
+	public static void main(String[] rgs) {
+		Scanner cf=new Scanner(System.in);
+		
+		System.out.print("Mode:");
+		String mode = cf.nextLine();
+		System.out.print("Temperature:");
+		double temp = cf.nextDouble();
+		
+		if(mode.equals("C_TO_F")) {
+			double result = TempConverter.celsiusToFahrenheit(temp);
+			System.out.println(result);
+		}
+		else if(mode.equals("F_TO_C")) {
+			double result = TempConverter.fahrenheitToCelsius(temp);
+			System.out.println(result);
+		}
+
+	}
+
+}
+
+class TempConverter {
+	
+	public static double celsiusToFahrenheit(double celsius) {
+		return (celsius * 9.0 / 5.0) + 32 ;
+	}
+	
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		return (fahrenheit - 32) * 5.0 / 9.0 ;
+	}
+}
