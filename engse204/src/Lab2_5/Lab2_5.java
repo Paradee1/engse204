a4ebcb58566d9d23de9bb83b601bc03f1fdc95dd
paradee1 <paradee5759@gmail.com>
2025-11-30 14:56:15 +0700

    rr

--- engse204/src/Lab2_5/Lab2_5.java
+++ engse204/src/Lab2_5/Lab2_5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Lab2_5 {
 
-	public static void main(String[] args) {
+	public static void main(String[] rgs) {
 		Scanner wh=new Scanner(System.in);
 		
 		System.out.print("Width:");
