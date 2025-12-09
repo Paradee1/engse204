685f7c299e61d848a40f474ee0feecec61e641b8
paradee1 <paradee5759@gmail.com>
2025-12-10 00:05:04 +0700

    134

---
+++ engse204/src/Lab3_3/Lab3_3.java
@@ -0,0 +1,41 @@
+package Lab3_3;
+
+import java.util.Scanner;
+
+public class Lab3_3 {
+
+	public static void main(String[] rgs) {
+		Scanner ps = new Scanner(System.in);
+		
+		String StartPass = ps.nextLine();
+		String NewPass = ps.nextLine();
+		
+		User pt = new User(StartPass);
+		pt.setPassword(NewPass);
+		System.out.println(pt.getPassword());
+
+	}
+
+}
+
+class User {
+	private String password ;
+	
+	public User(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String newPassword) {
+		if(newPassword.length() >= 8) {
+			this.password = newPassword ;
+			System.out.println( "Password update." );
+		}
+		else {
+			System.out.println("Password is too short.");
+		}
+	}
+}
