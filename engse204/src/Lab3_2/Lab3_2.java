25a246e958a80bea153c7cf70fb16950e21dc82c
paradee1 <paradee5759@gmail.com>
2025-12-09 22:12:40 +0700

    32

---
+++ engse204/src/Lab3_2/Lab3_2.java
@@ -0,0 +1,37 @@
+package Lab3_2;
+
+import java.util.Scanner;
+
+public class Lab3_2 {
+
+	public static void main(String[] rgs) {
+		Scanner in = new Scanner(System.in);
+		
+		int startScore = in.nextInt();
+
+		int updateScoer  = in.nextInt();
+		
+		Player py = new Player(startScore);
+		
+		py.setScore(updateScoer);
+		System.out.println(py.getScore());
+		
+	}
+
+}
+
+class Player {
+	private int score;
+	
+	public Player(int score) {
+		this.score =score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int newScore) {
+		score = newScore;
+	}
+}
