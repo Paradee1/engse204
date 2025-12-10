fe3a50cfe3a74ca5572329c4b3c372c743051351
paradee1 <paradee5759@gmail.com>
2025-12-10 21:07:57 +0700

    q

---
+++ engse204/src/Lab3_7/Lab3_7.java
@@ -0,0 +1,46 @@
+package Lab3_7;
+
+import java.util.Scanner;
+
+public class Lab3_7 {
+
+	public static void main(String[] rgs) {
+		Scanner dp = new Scanner(System.in);
+		
+		String id = dp.nextLine();
+		String depart = dp.nextLine();
+		String newdepart = dp.nextLine();
+		
+		Employee em = new Employee(id, depart);
+		
+		em.setDepartment(newdepart);
+		em.getEmployeeId();
+		em.getDepartment();
+		
+		System.out.println(em.getEmployeeId());
+		System.out.println(em.getDepartment());
+	}
+
+}
+
+class Employee {
+	private String employeeId;
+	private String department;
+	
+	public Employee(String employeeId, String department) {
+		this.employeeId = employeeId;
+		this.department = department;
+	}
+	
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public void setDepartment(String newDepartment) {
+		department = newDepartment;
+	}
+}
