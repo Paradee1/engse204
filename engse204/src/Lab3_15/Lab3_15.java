e8a9b7c0ee79d98f7cef2304ef666cae598a1eef
paradee1 <paradee5759@gmail.com>
2025-12-12 21:17:49 +0700

    asdasdasdas

--- engse204/src/Lab3_15/Lab3_15.java
+++ engse204/src/Lab3_15/Lab3_15.java
@@ -5,14 +5,35 @@ import java.util.Scanner;
 public class Lab3_15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner df = new Scanner(System.in);
+		
+		String name = df.nextLine();
+		User nm = new User(name);
+		
+		int N = df.nextInt();
+		df.nextLine();
+		
+		for(int i = 0 ; i < N ; i++) {
+			String order = df.nextLine();
+			
+			if(order.equals("SET_POLICY")) {
+				int newmax = df.nextInt();
+				df.nextLine();
+				User.setPolicy(newmax);
+			}
+			else if(order.equals("LOGIN")) {
+				String pass = df.nextLine();
+
+				nm.login(pass);
+			}
+		}
 
 	}
 
 }
 
 class User {
-	private String username;
+	private String username = null;
 	private int failedAttempts = 0;
 	private boolean isLocked = false;
 	private static int maxAttempts = 3;
@@ -34,7 +55,7 @@ class User {
 	}
 	
 	public String getUsername() {
-		return username;
+		return username ;
 	}
 	
 	public boolean isLocked() {
@@ -45,5 +66,27 @@ class User {
 		return failedAttempts;
 	}
 	
-	
+	public void login(String password) {
+		if(isLocked == true) {
+			System.out.println("Account is locked.");
+		}
+		else if(isLocked == false) {
+			
+			if(password.equals("pass123")) {
+				failedAttempts = 0;
+				System.out.println("Login successful.");
+			}
+			else {
+				failedAttempts++;
+				if(failedAttempts >= maxAttempts) {
+					isLocked = true;
+					System.out.println("Login failed. Account locked..");
+				}
+				else {
+					int left = maxAttempts - failedAttempts;
+					System.out.println("Login failed. [" + left +"] attempts left.");
+				}
+			}
+		}
+	}
 }
