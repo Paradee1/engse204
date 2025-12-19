12afa36cc1b68a446e8e75d4e95d0c55bd0ddd3e
paradee1 <paradee5759@gmail.com>
2025-12-20 00:31:26 +0700

    as

---
+++ engse204/src/Lab4_15/Lab4_15.java
@@ -0,0 +1,108 @@
+package Lab4_15;
+
+import java.util.Scanner;
+
+public class Lab4_15 {
+
+	public static void main(String[] args) {
+		Scanner kl = new Scanner(System.in);
+		
+		int max = kl.nextInt();
+		kl.nextLine();
+		
+		AuditRecord.setPolicy(max);
+		
+		String Username = kl.nextLine();
+		int N = kl.nextInt();
+		kl.nextLine();
+		
+		AuditRecord ad = new AuditRecord(Username);
+		
+		for(int i = 0; i < N; i++) {
+			String Logs = kl.nextLine();
+			
+			if(Logs.equals("SET_POLICY")) {
+				int newmax = kl.nextInt();
+				kl.nextLine();
+				
+				AuditRecord.setPolicy(newmax);
+			}
+			else {
+				ad = ad.addMessages(Logs);
+			}
+			
+		}
+		
+		ad.displayLog();
+		kl.close();
+	}
+
+}
+
+class AuditRecord {
+	private final String user;
+	private final String[] logMessages;
+	private static int maxMessages = 3;
+	
+	public AuditRecord(String user) {
+		this(user, new String[0]);
+	}
+	
+	public AuditRecord(String user, String[] logs) {
+		this.user = user;
+		if(logs.length > maxMessages) {
+			this.logMessages = new String[maxMessages];
+			for(int i = 0; i < maxMessages; i++) {
+				this.logMessages[i] = logs[logs.length - maxMessages + i];
+			}
+		}
+		else {
+			this.logMessages = new String[logs.length];
+			for(int i = 0; i < logs.length; i++) {
+				this.logMessages[i] = logs[i];
+			}
+		}
+	}
+	
+	public static void setPolicy(int max) {
+		if(max > 0) {
+			maxMessages = max;
+			System.out.println("Policy set to " + maxMessages);
+		}
+		else {
+			System.out.println("Invalid policy.");
+		}
+	}
+	
+	public AuditRecord addMessages(String message) {
+	    if (this.logMessages.length >= maxMessages) {
+	        System.out.println("Log is full.");
+	        return this;
+	    }
+
+	    String[] newLogs = new String[this.logMessages.length + 1];
+
+	    for (int i = 0; i < this.logMessages.length; i++) {
+	        newLogs[i] = this.logMessages[i];
+	    }
+
+	    newLogs[newLogs.length - 1] = message;
+
+	    System.out.println(message + " added.");
+	    return new AuditRecord(this.user, newLogs);
+	}
+	
+	public void displayLog() {
+		System.out.print("User: " + user);
+		System.out.print(", Logs: " + logMessages.length + " [");
+		
+		for(int i = 0; i < logMessages.length; i++) {
+			System.out.print(logMessages[i]);
+			
+			if(i < logMessages.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.print("]");
+	}
+}
