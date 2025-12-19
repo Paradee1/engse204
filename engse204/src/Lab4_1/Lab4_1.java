b3a046f330477091ffd08759729e4254c842c64f
paradee1 <paradee5759@gmail.com>
2025-12-20 00:38:59 +0700

    411

--- engse204/src/Lab4_1/Lab4_1.java
+++ engse204/src/Lab4_1/Lab4_1.java
@@ -7,16 +7,21 @@ public class Lab4_1 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		//รับโหมด 1 หรือ 2
 		int mode = in.nextInt();
 		in.nextLine();
 		
 		if(mode == 1) {
-			String title = in.nextLine();
-			Book bk = new Book(title);
 			
+			//รับชื่อเรื่อง
+			String title = in.nextLine();
+			
+			Book bk = new Book(title);
 			bk.displayInfo();
 		}
 		else if(mode == 2) {
+			
+			//รับชื่อเรื่องและผู้แต่ง
 			String title = in.nextLine();
 			String author = in.nextLine();
 			
@@ -24,7 +29,7 @@ public class Lab4_1 {
 			bk.displayInfo();
 		}
 		
-		
+		in.close();
 	}
 
 }
@@ -33,17 +38,20 @@ class Book {
 	private String title;
 	private String author;
 	
+	// Constructor 
 	public Book(String title) {
 		this.title = title;
 		this.author = "Unknown";
 	}
 	
+	// Constructor 
 	public Book(String title, String author) {
 		this.title = title;
 		this.author = author;
 	}
 	
 	public void displayInfo() {
+		System.out.println("----------------------------------------------------");
 		System.out.println("Title: " + title + ", Author: " + author);
 	}
 }
